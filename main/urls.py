9b91d908604390c21cb3b5af12f318656e20a51a
jacobmoore <jakemoore271@outlook.com>
2022-03-02 05:31:59 -0500

    removed lesson_example and renamed requirments.txt to requirements.txt. Also, did some additional refactoring

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('topics', views.topics, name='topics'),
-    path('lesson_example', views.lesson_example, name='lesson_example'),
     path('lessons/(<lesson_id>\d+)', views.lessons, name='lessons'),
     path('lesson_quizzes/(<lesson_id>\d+)', views.lesson_quizzes, name='lesson_quizzes')
 ]
--- main/views.py
+++ main/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Lesson
 
-# for testing
-from django.http import HttpResponse
-
-
 def home(request):
     return render(request, 'main/home.html')
 
@@ -13,9 +9,6 @@ def topics(request):
     all_topics = Topic.objects.all()
     context = {'topics': all_topics}
     return render(request, 'main/topics.html', context)
-
-def lesson_example(request):
-    return render(request, 'main/lesson_example.html')
 
 def lessons(request, lesson_id):
     lesson = Lesson.objects.get(id=lesson_id)
