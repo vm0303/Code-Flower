b472512d054cec863691c30d63db7383776f9c9f
jacobmoore <jakemoore271@outlook.com>
2022-03-10 20:53:45 -0500

    updated

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('topics', views.topics, name='topics'),
     path('lessons/(<lesson_id>\d+)', views.lessons, name='lessons'),
-    path('lesson_quizzes/(<lesson_id>\d+)', views.lesson_quizzes, name='lesson_quizzes')
+    path('lesson_quizzes/(<lesson_id>\d+)', views.lesson_quizzes, name='lesson_quizzes'),
+    path('validate/lesson_quiz', views.quiz_processing, name='quiz_processing')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Lesson
+from django.http import JsonResponse
 
 def home(request):
     return render(request, 'main/home.html')
@@ -19,3 +20,6 @@ def lesson_quizzes(request, lesson_id):
     lesson = Lesson.objects.get(id=lesson_id)
     context = {'lesson': lesson}
     return render(request, 'main/lesson_quizzes.html', context)
+
+def quiz_processing(request):
+    return JsonResponse({'love': 'is not defined'})
