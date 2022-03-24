d25d19df97c2066da6167b8a2ac7efb439ab43a6
jacobmoore <jakemoore271@outlook.com>
2022-03-24 03:15:38 -0400

    trying to fix static file issue with production

--- CodeFlower/settings.py
+++ CodeFlower/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", # this line is new and untested
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
