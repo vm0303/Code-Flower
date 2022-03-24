f716f2953cecdd2d07cd5daa536093d690136d03
jacobmoore <jakemoore271@outlook.com>
2022-03-24 02:47:29 -0400

    trying to fix error with production

--- CodeFlower/settings.py
+++ CodeFlower/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 
 # To support Postgres db on Heroku
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
