93c237faf62e8606e391651f50657541434acac5
jacobmoore <jakemoore271@outlook.com>
2022-04-06 18:17:21 -0400

    Lesson comment model added

--- main/models.py
+++ main/models.py
@@ -98,3 +98,12 @@ class UserLessonProgress(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['user', 'lesson'], name='combination of lesson and user is unique')
         ]
+
+class LessonComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.lesson.name
