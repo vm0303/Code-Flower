6f0c42f3213fc232294a7f353f946710fb9ea74d
jacobmoore <jakemoore271@outlook.com>
2022-04-17 00:00:46 -0400

    added instructor tag next to username in the comment section and change the name on the default admin page

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.site_title = "CodeFlower"
+admin.site.site_header = "CodeFlower"
+admin.site.index_title = "CodeFlower"
+
 admin.site.register(Topic)
 admin.site.register(Lesson)
 admin.site.register(LessonQuestion)
